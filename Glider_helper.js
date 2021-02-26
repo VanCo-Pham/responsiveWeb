c43e4014d5801581fa3f511de0fec50cf2354d65
CoPham <copv1110@gmail.com>
2021-02-26 14:50:20 +0700

    Updates

--- Glider_helper.js
+++ Glider_helper.js
@@ -1,7 +1,9 @@
+
+  
 window.addEventListener('load',function(){
     var gliderWhy = new Glider(document.getElementById('glider-why'), {
-      slidesToScroll: 2,
-      slidesToShow: 2,
+      slidesToScroll: 1,
+      slidesToShow: 1,
       draggable: true,
       dots: '#dots-why',
       arrows: {
@@ -10,3 +12,17 @@ window.addEventListener('load',function(){
       }
     });
   })
+
+
+
+// new Glider(document.querySelector('.glider_product'), {
+
+//     slidesToShow: 3,
+//     draggable: true,
+//     dots: '#dots_prodcut',
+//     rewind: true,
+//     arrows: {
+//         prev : '.glider_product-prev',
+//         next: '.glider_product-next'
+//     }
+// })
